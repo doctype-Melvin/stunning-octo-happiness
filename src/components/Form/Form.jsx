2423a0acaf07f70107836a14a657d1b64339c762
doctype-Melvin <doctype.melvin@gmail.com>
2023-04-05 18:25:12 +0200

    adds change handler function for form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [entryObject, setEntryObject] = useState({
+    motto: "",
+    notes: "",
+  });
+
+  const handleChange = (event) => console.log(event.target.value);
+
   return (
     <>
       <h2>NEW ENTRY</h2>
       <label htmlFor="motto">MOTTO</label>
-      <input type="text" name="motto" id="motto" />
+      <input type="text" name="motto" id="motto" onChange={handleChange} />
       <label htmlFor="notes">NOTES</label>
-      <textarea name="notes" id="notes" cols="30" rows="3"></textarea>
+      <textarea
+        name="notes"
+        id="notes"
+        cols="30"
+        rows="3"
+        onChange={handleChange}
+      ></textarea>
       <button type="button">Create</button>
     </>
   );
