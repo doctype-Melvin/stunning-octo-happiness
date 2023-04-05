3354d618ba2fade867ab81d01eb24e8588304251
doctype-Melvin <doctype.melvin@gmail.com>
2023-04-05 18:09:48 +0200

    adds Header and Form component directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+    <Header />
+    <Main>
+    <Main />
+    <Footer />
+  </div>
+  );
 }
