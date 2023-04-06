2cdeb8a6c1c3270bf093f5df27ff407f1770c3ba
doctype-Melvin <doctype.melvin@gmail.com>
2023-04-06 14:36:26 +0200

    adds Card component and rendering

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
 import Entries from "../Entries/Entries";
 import Form from "../Form/Form";
+import Card from "../Card/Card";
 
 // localStorage.clear();
 export default function Main() {
   const [newEntry, setNewEntry] = useState("");
   const [allEntries, setAllEntries] = useState(0);
-  const [favorites, setFavorites] = useState("");
+  const [favorites, setFavorites] = useState(0);
+  const [entriesArray, setEntriesArray] = useState("");
 
   useEffect(() => {
-    setAllEntries(JSON.parse(localStorage.getItem("journal")).length);
+    if (localStorage.getItem("journal")) {
+      let dataOnRender = JSON.parse(localStorage.getItem("journal"));
+      setAllEntries(dataOnRender.length);
+      setFavorites(
+        dataOnRender.filter((entry) => entry.favorite === true).length
+      );
+      setEntriesArray(dataOnRender);
+    }
+  }, []);
+
+  useEffect(() => {
     if (!localStorage.getItem("journal")) {
       localStorage.setItem("journal", JSON.stringify([]));
     } else if (newEntry) {
@@ -17,6 +29,10 @@ export default function Main() {
       let updatedData = [...currentData, newEntry];
       localStorage.setItem("journal", JSON.stringify(updatedData));
       setAllEntries(JSON.parse(localStorage.getItem("journal")).length);
+      setFavorites(
+        updatedData.filter((entry) => entry.favorite === true).length
+      );
+      setEntriesArray(updatedData);
     }
   }, [newEntry]);
 
@@ -28,13 +44,27 @@ export default function Main() {
     set the favorites state
     pass the length to Entries component
     */
-    console.log(currentData);
   });
 
   return (
     <div>
       <Form newEntry={newEntry} setNewEntry={setNewEntry} />
-      <Entries total={allEntries}></Entries>
+      <Entries total={allEntries} favorites={favorites}>
+        <button
+          type="button"
+          onClick={() => {
+            localStorage.clear();
+            localStorage.setItem("journal", JSON.stringify([]));
+            setAllEntries(0);
+            setNewEntry("");
+          }}
+        >
+          Delete All
+        </button>
+        {entriesArray.map((entry) => (
+          <Card motto={entry.motto} notes={entry.notes} key={entry.id} />
+        ))}
+      </Entries>
     </div>
   );
 }
