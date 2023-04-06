8090946e6ad60436f412a8c15b2e5ab877088c9f
doctype-Melvin <doctype.melvin@gmail.com>
2023-04-06 12:36:45 +0200

    creates empty array in localStorage and new objects from form submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,47 +2,44 @@ import { nanoid } from "nanoid";
 import { useState } from "react";
 
 export default function Form(props) {
-  const { newEntry, setNewEntry } = props;
-  const [entryObject, setEntryObject] = useState({
-    motto: "",
-    notes: "",
-  });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setEntryObject((prevState) => {
-      return {
-        ...prevState,
-        [name]: value,
-        id: nanoid(),
-      };
-    });
-  };
+  const { setNewEntry } = props;
+  const [motto, setMotto] = useState("");
+  const [notes, setNotes] = useState("");
 
   // Submit this to local storage
-  const handleCreateClick = () => {
-    setNewEntry((prevState) => {
-      return [...prevState, entryObject];
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNewEntry({
+      motto,
+      notes,
+      id: nanoid(),
+      favorite: false,
     });
-    console.log(entryObject);
+    setMotto("");
+    setNotes("");
   };
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <h2>NEW ENTRY</h2>
       <label htmlFor="motto">MOTTO</label>
-      <input type="text" name="motto" id="motto" onChange={handleChange} />
+      <input
+        type="text"
+        name="motto"
+        id="motto"
+        onChange={(event) => setMotto(event.target.value)}
+        value={motto}
+      />
       <label htmlFor="notes">NOTES</label>
       <textarea
         name="notes"
         id="notes"
         cols="30"
         rows="3"
-        onChange={handleChange}
+        onChange={(event) => setNotes(event.target.value)}
+        value={notes}
       ></textarea>
-      <button type="button" onClick={handleCreateClick}>
-        Create
-      </button>
-    </>
+      <button type="submit">Create</button>
+    </form>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from "react";
 import Entries from "../Entries/Entries";
 import Form from "../Form/Form";
 
+// localStorage.clear();
 export default function Main() {
-  const [newEntry, setNewEntry] = useState([]);
+  const [newEntry, setNewEntry] = useState("");
 
   useEffect(() => {
-    if (localStorage.length < 1) {
-      localStorage.setItem("journal", JSON.stringify(newEntry));
-    } else {
-      let storageData = JSON.parse(localStorage.getItem("journal"));
-
-      console.log(localStorage);
+    if (!localStorage.getItem("journal")) {
+      localStorage.setItem("journal", JSON.stringify([]));
+    } else if (newEntry) {
+      let currentData = JSON.parse(localStorage.getItem("journal"));
+      let updatedData = [...currentData, newEntry];
+      localStorage.setItem("journal", JSON.stringify(updatedData));
     }
   }, [newEntry]);
+
+  // useEffect(() => {
+  //   let currentData = JSON.parse(localStorage.getItem("journal"));
+  //   let updatedData = [...currentData, newEntry];
+  //   localStorage.setItem("journal", JSON.stringify(updatedData));
+  // });
 
   return (
     <div>
